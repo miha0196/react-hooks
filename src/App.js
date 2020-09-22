76105c4ad0dcf0facf3ab4d052d8b1eee6de1c93
Michael Khakhel <miha0196@mail.ru>
2020-09-22 21:01:56 +0300

    completed project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Navbar} from './components/Navbar';
+import {About} from './pages/About';
+import {Home} from './pages/Home';
+import {Profile} from './pages/Profile';
+import {Alert} from './components/Alert';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import {AlertState} from './context/alert/AlertState';
+import {GithubState} from './context/github/GithubState';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GithubState>
+      <AlertState>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container pt-4">
+            <Alert alert={{text: 'Test alert'}} />
+            <Switch>
+              <Route path='/' exact component={Home} />
+              <Route path='/about' component={About} />
+              <Route path='/profile/:name' component={Profile} />
+            </Switch>
+          </div>
+        </BrowserRouter>
+      </AlertState>
+    </GithubState>
   );
 }
 
